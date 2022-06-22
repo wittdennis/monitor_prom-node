f716df1cd3a3cd9f98fae2dfec549f548f9ee399
Dennis Witt <witt.dennis@outlook.com>
2022-06-22 22:01:21 +0200

    cert and password argument

--- src/intall.sh
+++ src/intall.sh
@@ -5,6 +5,24 @@ if [ "$EUID" -ne 0 ]
   exit
 fi
 
+while getopts ":c:p:" opt; do
+  case $opt in
+    c) GENERATE_CERT="$OPTARG"
+    ;;
+    p) PASSWORD="$OPTARG"
+    ;;
+    \?) echo "Invalid option -$OPTARG" >&2
+    exit 1
+    ;;
+  esac
+
+  case $OPTARG in
+    -*) echo "Option $opt needs a valid argument"
+    exit 1
+    ;;
+  esac
+done
+
 SCRIPT=$(readlink -f "$0")
 SCRIPT_PATH=$(dirname "$SCRIPT")
 NODE_EXPORTER_VERSION="1.3.1"
@@ -13,7 +31,6 @@ BIN_DIRECTORY="/usr/local/bin"
 BIN="node_exporter"
 CONF_DIRECTORY="/etc/node-exporter"
 ARCH="linux-amd64"
-RAND_PW=$(< /dev/urandom tr -dc A-Za-z0-9 | head -c32;)
 PORT="9100"
 
 echo "Creating user '${NODE_EXPORTER_USER}' if not present"
@@ -30,16 +47,29 @@ rm -rf node_exporter-${NODE_EXPORTER_VERSION}.${ARCH}*
 
 # config
 mkdir ${CONF_DIRECTORY}
-# self-signed localhost cert
-openssl req -new -newkey rsa:2048 -days 365 -nodes -x509 -keyout node_exporter.key -out node_exporter.crt -subj "/CN=localhost" -addext "subjectAltName = DNS:localhost"
-mv node_exporter.* ${CONF_DIRECTORY}/
+
+if [ -z "${GENERATE_CERT}" ] || [ "${GENERATE_CERT}" != "false" ]
+then
+  echo "Generating self-signed certificate"
+  # self-signed localhost cert
+  openssl req -new -newkey rsa:2048 -days 365 -nodes -x509 -keyout node_exporter.key -out node_exporter.crt -subj "/CN=localhost" -addext "subjectAltName = DNS:localhost"
+  mv node_exporter.* ${CONF_DIRECTORY}/
+fi
+
 cp ${SCRIPT_PATH}/config.yml ${CONF_DIRECTORY}/
 cp ${SCRIPT_PATH}/node_exporter.service /etc/systemd/system/
 
-# change values of config file and systemd service
-echo "Generating password for basic auth"
-PASSWD=$(htpasswd -nbBC 10 "" ${RAND_PW} | tr -d ':\n')
+RAND_PW=0
+if [ -z "${PASSWORD}" ]
+then
+  echo "Generating password for basic auth"
+  PASSWORD=$(< /dev/urandom tr -dc A-Za-z0-9 | head -c32;)
+  RAND_PW=1
+fi
 
+
+# change values of config file and systemd service
+PASSWD=$(htpasswd -nbBC 10 "" ${PASSWORD} | tr -d ':\n')
 sed -i "s@\${PW}@${PASSWD}@g" "${CONF_DIRECTORY}/config.yml"
 sed -i "s@\${NODE_EXPORTER_USER}@${NODE_EXPORTER_USER}@g" "/etc/systemd/system/node_exporter.service"
 sed -i "s@\${BIN_DIRECTORY}@${BIN_DIRECTORY}@g" "/etc/systemd/system/node_exporter.service"
@@ -56,7 +86,12 @@ systemctl restart node_exporter
 echo ""
 echo "Started node_exporter on: https://localhost:${PORT}"
 echo "Use the following user and password to connect"
-echo "prometheus: ${RAND_PW}"
+if [ ${RAND_PW} -eq 1 ]
+then
+  echo "prometheus: ${PASSWORD}"
+else
+  echo "prometheus: ***********"
+fi
 
 echo ""
 echo "Done!"
