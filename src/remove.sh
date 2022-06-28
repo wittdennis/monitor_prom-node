26d1e87e3e435f8bdd96b5b4f86ac9a4ab4b1b41
Dennis Witt <witt.dennis@outlook.com>
2022-06-28 20:48:24 +0200

    remove without error

--- src/remove.sh
+++ src/remove.sh
@@ -15,16 +15,16 @@ CONF_DIRECTORY="/etc/node-exporter"
 ARCH="linux-amd64"
 
 echo "Removing service"
-systemctl stop node_exporter
-systemctl disable node_exporter
-rm /etc/systemd/system/node_exporter.service
+systemctl stop node_exporter.service > /dev/null 2>&1
+systemctl disable node_exporter.service > /dev/null 2>&1
+rm -f /etc/systemd/system/node_exporter.service
 systemctl daemon-reload
 
 echo "Removing config directory"
 rm -rf ${CONF_DIRECTORY}/
 
 echo "Removing binaries"
-rm ${BIN_DIRECTORY}/${BIN}
+rm -f ${BIN_DIRECTORY}/${BIN}
 
 echo "Removing user '${NODE_EXPORTER_USER}'"
 userdel -f -r ${NODE_EXPORTER_USER} > /dev/null 2>&1 
